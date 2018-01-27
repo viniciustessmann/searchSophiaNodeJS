9eec8b1b999a6c91485b431437b305682d7f02fc
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2018-01-27 17:03:32 -0200

    Start project

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-var http = require('http');
+var http       = require('http'),
+    formidable = require('formidable'),
+    fs         = require('fs');
 
 var server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-type": "text/plan"});
-    res.write('Helo world!');
-    res.end();
+
+    if (req.url == '/fileupload') {
+
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            res.writeHead(200, {'content-type': 'text/plain'});
+            var oldpath = files.filetoupload.path;
+            var newpath = '/Applications/XAMPP/htdocs/search-sophia-node-js/files/input.txt'
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } else {
+        
+        res.write("<h1>Where's Sophia</h1>");
+        res.write("<label>Input TXT</label>");
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br><br>');
+        res.write('<input type="submit" value="Send">');
+        res.write('</form>');
+        res.end();
+    }
+    
 });
 
 server.listen(3000, function(){
